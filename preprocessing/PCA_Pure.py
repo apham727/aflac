806cb2c958e986c2688f06a70550fdfe9a1a97d8
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2019-12-16 14:08:57 -0600

    PCA Visualization Script
    Performs PCA on length 22050 FFTs of wav files in a given directory.

--- preprocessing/PCA_Pure.py
+++ preprocessing/PCA_Pure.py
@@ -2,7 +2,7 @@ import os
 import glob
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 from sklearn.preprocessing import StandardScaler
 from scipy.io import wavfile
 
